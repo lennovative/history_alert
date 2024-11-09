806306c6e8766b93056615018bc7f66d106b611e
Hubert Sidorowicz <148703988+hubertsid@users.noreply.github.com>
2024-11-09 17:09:14 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        '': ['blacklisted_words.bin'],  # Adjust the path as necessary
+    },
     py_modules=['main', 'show_todays_history', 'get_database', 'update_events'],
     entry_points={
         'console_scripts': [
